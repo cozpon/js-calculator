13260cff38640f549827a06ee57b12397521f2e1
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-08-26 18:50:03 -1000

    First commit

--- calculator.js
+++ calculator.js
@@ -5,6 +5,17 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+ var calculatorModule = (function(){
+
+   var memory = 0
+   var total = 0
+
+   var calculator = {
+    load: load,
+    getTotal: getTotal,
+  
+
+};
 
 
   /**
@@ -12,13 +23,18 @@
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+   function load(x){
+    total = x;
+    return total;
+   };
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+   function getTotal(){
+    return total;
+   };
 
   /**
    * Sums the value passed in with `total`
@@ -30,36 +46,39 @@
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+ 
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+  
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
-
+  
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+   
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+   
 
   /**
    * Clear the value stored at `memory`
    */
+  
 
   /**
    * Validation
    */
+ 
 
+   return calculator;
+ });
